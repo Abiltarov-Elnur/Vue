7bc7757944d36542b916b93729bc7b39dcca0c36
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-05-14 23:27:40 +0300

    Задание_54

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,27 @@
 <template>
-	<button v-bind:disabled="isDisabled">btn</button>
+	<ul>
+		<li v-for="(item, index) in items" :key="index">
+			{{ item }}
+			<button @click="removeItem(index)">remove</button>
+		</li>
+	</ul>
 </template>
 
 <script>
 export default {
   data() {
-	return {
-		isDisabled: true,
-	}
-},
+    return {
+      items: ['a', 'b', 'c', 'd', 'e'],
+    };
+  },
+  methods: {
+    removeItem(index) {
+      this.items.splice(index, 1);
+    },
+  },
 };
 </script>
+
 
 <style scoped>
 p {
