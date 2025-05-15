94faf5dd1eb8ca1e537c5d775ab3cc63412dd5f5
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-05-15 22:32:51 +0300

    Задание_65

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,47 +1,20 @@
 <template>
-	<User
-		v-for="user in users"
-		:name="user.name"
-		:surn="user.surn"
-		:key="user.id"
-	/>
+  <div>
+    <button @click="handleFirst">Вызвать первую</button>
+    <button @click="handleSecond">Вызвать вторую</button>
+  </div>
 </template>
 
 <script>
-	export default {
-		props: ['name', 'surn'],
-		data() {
-	return {
-		users: [
-			{
-				id: 1,
-				name: 'name1',
-				surn: 'surn1'
-			},
-			{
-				id: 2,
-				name: 'name2',
-				surn: 'surn2'
-			},
-			{
-				id: 3,
-				name: 'name3',
-				surn: 'surn3'
-			},
-		],
-	}
-}
-	}
+export default {
+  emits: ['first-action', 'second-action'],
+  methods: {
+    handleFirst() {
+      this.$emit('first-action');
+    },
+    handleSecond() {
+      this.$emit('second-action');
+    },
+  },
+};
 </script>
-
-
-<style scoped>
-p {
-  color: red;
-}
-
-/* Класс done перечеркивает текст */
-p.done {
-  text-decoration: line-through;
-}
-</style>
