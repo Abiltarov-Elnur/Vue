3f44e7fe75db1c9331201575d8d067db48a7b4ac
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-05-15 22:30:06 +0300

    Задание_63

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,14 +1,34 @@
 <template>
-	<User :name="name" :surn="surn" />
+	<User
+		v-for="user in users"
+		:name="user.name"
+		:surn="user.surn"
+		:key="user.id"
+	/>
 </template>
 
 <script>
 	export default {
-		
+		props: ['name', 'surn'],
 		data() {
 	return {
-		name: 'john',
-		surn: 'smit',
+		users: [
+			{
+				id: 1,
+				name: 'name1',
+				surn: 'surn1'
+			},
+			{
+				id: 2,
+				name: 'name2',
+				surn: 'surn2'
+			},
+			{
+				id: 3,
+				name: 'name3',
+				surn: 'surn3'
+			},
+		],
 	}
 }
 	}
