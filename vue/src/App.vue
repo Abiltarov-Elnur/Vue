35c55ecf3720df9f8b0e30b21e6bf96f4aeb2020
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-05-15 22:36:12 +0300

    Задание_69

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,41 @@
 <template>
-  <button @click="$emit('hello')">Сказать привет</button>
+  <div>
+    <Employee
+      v-for="user in users"
+      :key="user.id"
+      :id="user.id"
+      :name="user.name"
+      :surn="user.surn"
+      @change="change"
+    />
+  </div>
 </template>
 
 <script>
+import Employee from './Employee.vue';
+
 export default {
-  emits: ['hello'],
+  components: { Employee },
+  data() {
+    return {
+      users: [
+        { id: 1, name: 'Alice', surn: 'Smith' },
+        { id: 2, name: 'Bob', surn: 'Johnson' },
+      ],
+    };
+  },
+  methods: {
+    change(id, name, surn) {
+      this.users = this.users.map(user => {
+        if (user.id === id) {
+          user.name = name;
+          user.surn = surn;
+        }
+        return user;
+      });
+    },
+  },
 };
 </script>
+
+
