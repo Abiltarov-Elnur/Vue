2972cd592e1f66c3061c4a78e9bec344b7d69cca
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-05-15 22:35:06 +0300

    Задание_67

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,20 +1,9 @@
 <template>
-  <div>
-    <button @click="handleFirst">Вызвать первую</button>
-    <button @click="handleSecond">Вызвать вторую</button>
-  </div>
+  <button @click="$emit('hello')">Сказать привет</button>
 </template>
 
 <script>
 export default {
-  emits: ['first-action', 'second-action'],
-  methods: {
-    handleFirst() {
-      this.$emit('first-action');
-    },
-    handleSecond() {
-      this.$emit('second-action');
-    },
-  },
+  emits: ['hello'],
 };
 </script>
